c8c9aab272fb8ef627d3ab2f4465bfe8eaef470e
Pedro <pedrocarvi@outlook.com>
2026-06-28 16:16:16 -0300

    Corrijo imports index ts fix vercel build 5

--- api-gymhour/api/index.ts
+++ api-gymhour/api/index.ts
@@ -1,10 +1,11 @@
 // import app from '../src/app.js'
 // export default app;
+import type { NextFunction, Request, Response } from "express";
 import app from "../dist/app.js";     // tu Express "app"
 
-export default function handler(req, res) {
+export default function handler(req: Request, res: Response) {
   return new Promise<void>((resolve, reject) => {
-    app(req, res, (err) => {
+    app(req, res, (err: Parameters<NextFunction>[0]) => {
       if (err) return reject(err);
       resolve();
     });
